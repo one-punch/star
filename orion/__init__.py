ebe66a370887d76fb0d9aebc12109578bc97352e
scalaview <chailink100@gmail.com>
2016-02-17 18:15:03 +0800

    add init

---
+++ orion/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""description here"""
+
+from __future__ import generators
+
+__version__ = "0.01"
+__author__ = [
+    "benko <chailink100@gmail.com>"
+]
+__license__ = "public domain"
+__contributors__ = ""
+
+# import utils, db, net, wsgi, http, webapi, httpserver, debugerror
+# import template, form
+
+# import session
+
+# from utils import *
+# from db import *
+# from net import *
+# from wsgi import *
+# from http import *
+# from webapi import *
+# from httpserver import *
+# from debugerror import *
+# from application import *
+# from browser import *
+# try:
+#     import webopenid as openid
+# except ImportError:
+#     pass # requires openid module
