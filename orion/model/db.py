c34647e2fbb7b30297756a9d8138db8c49fe7b8e
scalaview <chailink100@gmail.com>
2016-02-29 17:21:52 +0800

    add bilibili api

--- orion/model/db.py
+++ orion/model/db.py
@@ -174,6 +174,41 @@ class MovieQueue(BaseModel):
 		db_table = prefix + "movie_queue"
 
 
+class BilibiliMovie(BaseModel):
+	id = PrimaryKeyField()
+	douban_id = IntegerField()
+	avid = IntegerField()
+	author = CharField()
+	typename = CharField()
+	arcurl = CharField()
+
+
+"type": "video",
+            "id": 3658407,
+            "author": "无敌赵大宝941",
+            "mid": 21787261,
+            "typename": "电影相关",
+            "arcurl": "http://www.bilibili.com/video/av3658407/",
+            "aid": "3658407",
+            "description": "自制 无论他是韦格纳还是莉莉，格尔达都爱他，他是她的缪斯也是她的爱人\r\n这个电影很让人感动也让人揪心，是一部非常棒的电影",
+            "title": "丹麦女孩",
+            "arcrank": "0",
+            "pic": "http://i0.hdslb.com/video/a2/a27341c8157ed529408165016f3de81b.jpg",
+            "play": 32772,
+            "video_review": 34,
+            "favorites": 593,
+            "tag": "电影剪辑,丹麦女孩",
+            "review": 12,
+            "pubdate": 1453656091
+
+	created_at = DateTimeField(default=datetime.datetime.now)
+	updated_at = DateTimeField(default=datetime.datetime.now)
+
+	class Meta:
+		db_table = prefix + "bilibili_movies"
+
+
+
 def init_db():
 	__all__ = [
 		Image,
