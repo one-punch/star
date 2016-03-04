ca1ae4df3965a91fff1556956fc180dacd38739c
benko <chailink100@gmail.com>
2016-03-05 00:19:16 +0800

    add cookie

--- orion/bellatrix.py
+++ orion/bellatrix.py
@@ -21,7 +21,7 @@ class Planet:
 		'Accept': 'text/html, application/xhtml+xml, */*',
 		'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
 		'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
-		'Cookie': ''
+		'Cookie': config.options.cookie
 		}):
 		self.head = head
 		self.opener = urllib.request.build_opener()
